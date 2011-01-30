f004ed516c83040b5d48c83b2c11572f53dd6ba9
wittekm <wittekm@umich.edu>
2011-01-30 15:58:00 -0500

    fixed secondviewcontroller

--- Classes/SecondViewController.m
+++ Classes/SecondViewController.m
@@ -81,7 +81,8 @@
 	NSLog(date); 
 	NSString * price = [NSString stringWithFormat:@"%d", [[data price] integerValue]];
 	NSString * gallons = [NSString stringWithFormat:@"%d", [[data gallons] integerValue]];
-	cell.textLabel.text = [NSString stringWithFormat:@"%@, %@", date, [[data location] coordinate]];
+	
+	cell.textLabel.text = [NSString stringWithFormat:@"%@, %@", date, [[data location] description]];
 	cell.detailTextLabel.text = [NSString stringWithFormat:@"$%@, %@ gal", price, gallons];
 	
     return cell;
